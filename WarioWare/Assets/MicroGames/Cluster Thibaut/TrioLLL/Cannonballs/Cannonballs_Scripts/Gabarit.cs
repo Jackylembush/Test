cd33b139771b0cab829b43139a1f8e9ad9ba7147
Louis Lefebvre <l.lefebvre@rubika-edu.com>
2020-12-05 18:38:46 +0100

    added gabarit aléatoire

--- WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Cannonballs/Cannonballs_Scripts/Gabarit.cs
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Cannonballs/Cannonballs_Scripts/Gabarit.cs
@@ -14,10 +14,17 @@ namespace TrioLLL
             public GameObject Player;
             private Rigidbody2D rb;
             public bool isPlayerOut = true;
+            public bool randomized = false;
+            public float min_x = -20f;
+            public float max_x = 20f;
+            public float min_y = -10f;
+            public float max_y = 10f;
+            public Vector2 nextPosition;
             // Start is called before the first frame update
             void Start()
             {
                 rb = GetComponent<Rigidbody2D>();
+                nextPosition = new Vector2(Random.Range(min_x, max_x), Random.Range(min_y, max_y));
             }
 
             // Update is called once per frame
@@ -28,7 +35,24 @@ namespace TrioLLL
 
             private void Move()
             {
-                rb.velocity = (Player.transform.position - transform.position).normalized * (baseSpeed * speedModifier);
+                if (randomized)
+                {
+                    if (Vector2.Distance(transform.position, nextPosition)<=0.01f)
+                    {
+                        nextPosition = new Vector2(Random.Range(min_x, max_x), Random.Range(min_y, max_y));
+                    }
+                    transform.position = Vector2.MoveTowards(transform.position, nextPosition, baseSpeed*speedModifier);
+                    //Vector2 newDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                    //Debug.Log(newDirection);
+                    //rb.velocity = newDirection.normalized * (baseSpeed * speedModifier);
+                    //Debug.Log(rb.velocity);
+                }
+                else
+                {
+                    rb.velocity = (Player.transform.position - transform.position).normalized * (baseSpeed * speedModifier);
+                }
+
+
             }
 
             private void OnTriggerStay2D(Collider2D collision)
