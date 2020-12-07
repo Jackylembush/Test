74c6daf7b3fc5539d5d5b5638e118d3790e0d8ab
Louis Vitrant <l.vitrant@rubika-edu.com>
2020-12-07 13:02:27 +0100

    Sprint_0712_Basho

---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Basho/Scripts/EnemyLife.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioLLL
+{
+    namespace Basho
+    {
+        /// <summary>
+        /// Louis Vitrant
+        /// </summary>
+        public class EnemyLife : TimedBehaviour
+        {
+            public GameObject enemyLevel1;
+            public GameObject enemyLevel2;
+            public GameObject enemyLevel3;
+            public SpriteRenderer backgroundLevel1;
+            public SpriteRenderer backgroundLevel2;
+            public SpriteRenderer backgroundLevel3;
+            public int chosenLevel;
+            public float enemyLifeBar;
+
+
+            public void Start()
+            {
+                if (chosenLevel == 1)
+                {
+                    enemyLifeBar = 12;
+                    enemyLevel1.SetActive(true);
+                    enemyLevel2.SetActive(false);
+                    enemyLevel3.SetActive(false);
+                    this.backgroundLevel1 = GetComponent<SpriteRenderer>();
+                }
+                else if(chosenLevel == 2)
+                {
+                    enemyLifeBar = 15;
+                    enemyLevel1.SetActive(false);
+                    enemyLevel2.SetActive(true);
+                    enemyLevel3.SetActive(false);
+                    this.backgroundLevel2 = GetComponent<SpriteRenderer>();
+                }
+                else if (chosenLevel == 3)
+                {
+                    enemyLifeBar =17;
+                    enemyLevel1.SetActive(false);
+                    enemyLevel2.SetActive(false);
+                    enemyLevel3.SetActive(true);
+                    this.backgroundLevel3 = GetComponent<SpriteRenderer>();
+                }
+                else
+                {
+                    enemyLifeBar = 10000;
+                    enemyLevel1.SetActive(false);
+                    enemyLevel2.SetActive(false);
+                    enemyLevel3.SetActive(false);
+                }
+            }
+
+            public void Damage()
+            {
+                enemyLifeBar--;
+            }
+
+            public void Update()
+            {
+            }
+        }
+    }
+}
---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Basho/Scripts/FightScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioLLL
+{
+    namespace Basho
+    {
+        /// <summary>
+        /// Louis Vitrant
+        /// </summary>
+        public class FightScript : TimedBehaviour
+        {
+            public GameObject leftRestedArm;
+            public GameObject rightRestedArm;
+            public GameObject leftActivateddArm;
+            public GameObject rightActivatedArm;
+            public float hitDuration;
+            public bool leftArmIsActivated = false;
+            public bool rightArmIsActivated = false;
+            public bool canUseLeftArm = true;
+            public bool canUseRightArm = true;
+
+            public EnemyLife enemyLife; 
+
+            public override void Start()
+            {
+                base.Start();
+                leftRestedArm.SetActive(true);
+                rightRestedArm.SetActive(true);
+                rightActivatedArm.SetActive(false);
+                leftActivateddArm.SetActive(false);
+                hitDuration = 60 / bpm / 5;
+            }
+
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate();
+            }
+
+            public void Update()
+            {
+                if (Input.GetButtonDown("Left_Bumper") && canUseLeftArm)
+                {
+                    StartCoroutine(ActivationLeftArm());
+                }
+
+                if (Input.GetButtonDown("Right_Bumper") && canUseRightArm)
+                {
+                    StartCoroutine(ActivationRightArm());
+                }
+
+                if (leftArmIsActivated == true)
+                {
+                    leftActivateddArm.SetActive(true);
+                    leftRestedArm.SetActive(false);
+                    canUseLeftArm = false;
+                }
+                else
+                {
+                    leftActivateddArm.SetActive(false);
+                    leftRestedArm.SetActive(true);
+                    canUseLeftArm = true;
+                }
+
+                if (rightArmIsActivated == true)
+                {
+                    rightActivatedArm.SetActive(true);
+                    rightRestedArm.SetActive(false);
+                    canUseRightArm = false;
+                }
+                else
+                {
+                    rightActivatedArm.SetActive(false);
+                    rightRestedArm.SetActive(true);
+                    canUseRightArm = true;
+                }
+            }
+
+            IEnumerator ActivationLeftArm()
+            {
+                leftArmIsActivated = true;
+                enemyLife.Damage();
+                yield return new WaitForSeconds(hitDuration);
+                leftArmIsActivated = false;
+            }
+
+            IEnumerator ActivationRightArm()
+            {
+                rightArmIsActivated = true;
+                enemyLife.Damage();
+                yield return new WaitForSeconds(hitDuration);
+                rightArmIsActivated = false;
+            }
+        }
+    }
+}
