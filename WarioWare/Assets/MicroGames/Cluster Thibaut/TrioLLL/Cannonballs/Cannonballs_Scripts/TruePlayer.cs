92412b4c37d5f40e8db05828a5cabdc5de76e491
Louis Lefebvre <l.lefebvre@rubika-edu.com>
2021-01-07 15:40:16 +0100

    whalla les anims intégrées

--- WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Cannonballs/Cannonballs_Scripts/TruePlayer.cs
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Cannonballs/Cannonballs_Scripts/TruePlayer.cs
@@ -17,8 +17,13 @@ namespace TrioLLL
             [SerializeField]
             private float bpmDiviser;
             private Rigidbody2D rb;
+            private Animator animator;
+            public GameObject Face;
+            public GameObject Dos;
+            
             public override void Start()
             {
+                animator = gameObject.GetComponent<Animator>();
                 speedModifier = bpm/bpmDiviser ;
                 base.Start(); //Do not erase this line!
                 rb = GetComponent<Rigidbody2D>();
@@ -30,6 +35,7 @@ namespace TrioLLL
 
                 base.FixedUpdate(); //Do not erase this line!
                 Move();
+
             }
 
             //TimedUpdate is called once every tick.
@@ -39,11 +45,36 @@ namespace TrioLLL
             }
             private void Move()
             {
-
                 float inputHorizontal = Input.GetAxis("Left_Joystick_X");
                 float inputVertical = Input.GetAxis("Left_Joystick_Y");
                 rb.velocity = new Vector2(inputHorizontal, inputVertical).normalized * (speed * speedModifier);
+
+                if (inputVertical > 0)
+                {
+                    Dos.SetActive(true);
+                    Face.SetActive(false);
+                    animator.SetBool("RunNorth", true);
+                    animator.SetBool("RunSouth", false);
+                    animator.SetBool("Immobile", false);
+                }
+                if (inputVertical < 0)
+                {
+                    Face.SetActive(true);
+                    Dos.SetActive(false);
+                    animator.SetBool("RunNorth",false);
+                    animator.SetBool("RunSouth",true);
+                    animator.SetBool("Immobile",false);
+                }
+                if (inputVertical == 0 && inputVertical == 0)
+                {
+                    Face.SetActive(true);
+                    Dos.SetActive(false);
+                    animator.SetBool("Immobile",true);
+                    animator.SetBool("RunNorth", false);
+                    animator.SetBool("RunSouth", false);
+                }
             }
+
         }
     }
 }
