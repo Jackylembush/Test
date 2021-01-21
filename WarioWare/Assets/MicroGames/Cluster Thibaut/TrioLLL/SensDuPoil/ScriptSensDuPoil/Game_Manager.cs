dd1a8b02a8a0b59f5c6db65ef080f4eac1bee6ae
Tim <t.huerre@rubika-edu.com>
2021-01-21 10:55:24 +0100

    Animation changes

--- WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/SensDuPoil/ScriptSensDuPoil/Game_Manager.cs
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/SensDuPoil/ScriptSensDuPoil/Game_Manager.cs
@@ -9,7 +9,7 @@ namespace LLL
     {
         public class Game_Manager : TimedBehaviour
         {
-            public enum Catstate { NEEDY, HAPPY, ANGRY, PET } //Les différents états du chat
+            public enum Catstate { NEEDY, PET, HAPPY, ANGRY} //Les différents états du chat
 
             [Header("Cat Mood")] //Etat actuel du chat
             Catstate currentCatState;
@@ -24,6 +24,8 @@ namespace LLL
             public Sprite Pet;
             public Sprite Happy;
             public Sprite Angry;
+
+            private Animator anim;
 
             public bool canPet;
 
@@ -42,7 +44,7 @@ namespace LLL
                 base.Start(); //Do not erase this line!
                 currentCatState = Catstate.NEEDY;
                 canPet = true;
-
+                anim = GetComponent<Animator>();
                 switch (currentDifficulty) //Permet de gérer la difficulté; Le nombre de PatPat requis change à chaque fois. C'est un nombre aléatoire dans une plage déterminée.
                 {
                     case Difficulty.EASY:
@@ -85,17 +87,18 @@ namespace LLL
                         StartCoroutine(PetTimer(0.2f));
                     }
                 }
+
+                anim.SetInteger("State", (int)currentCatState);
+
                 switch (currentCatState) //Permet de transitionner entre les différents états du chat
                 {
                     case Catstate.NEEDY: //Le chat réclame des PatPat
-
-                        gameObject.GetComponent<SpriteRenderer>().sprite = Needy;
 
                         //Afficher les sprites du chat needy
                         break;
 
                     case Catstate.PET: //Le chat reçois des PatPat
-                        gameObject.GetComponent<SpriteRenderer>().sprite = Pet;
+
                         Debug.Log(currentCatState);
                         //Afficher les sprites du pat pat
                         //Son de ronron
@@ -103,12 +106,11 @@ namespace LLL
 
                     case Catstate.HAPPY: //Le chat est heureux
                         Debug.Log(currentCatState);
-                        gameObject.GetComponent<SpriteRenderer>().sprite = Happy;
                         //Son de chat content
                         break;
 
                     case Catstate.ANGRY: //Le chat a recu trop de PatPat
-                        gameObject.GetComponent<SpriteRenderer>().sprite = Angry;
+
                         //Son de chat pas content
                         break;
                 }
